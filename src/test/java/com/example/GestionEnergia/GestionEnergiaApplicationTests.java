8cc2d0e03a851701217569a5eb45a3dca1193365
dxn1l <danielmorenorey2012@gmail.com>
2024-12-14 19:12:06 +0100

    Initial commit

---
+++ src/test/java/com/example/GestionEnergia/GestionEnergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionEnergia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEnergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
