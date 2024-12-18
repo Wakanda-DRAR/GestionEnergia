c21717b1456c9a3f0d89505a1261dbd1b2ce97f8
Roberquilez <159033774+Roberquilez@users.noreply.github.com>
2024-12-18 13:14:44 +0100

    Docker
    falta configurar cosas

--- src/main/java/com/example/GestionEnergia/GestionEnergiaApplication.java
+++ src/main/java/com/example/GestionEnergia/GestionEnergiaApplication.java
@@ -1,15 +1,14 @@
 package com.example.GestionEnergia;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EnergiaSostenibleApplication {
+public class GestionEnergiaApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EnergiaSostenibleApplication.class, args);
+		SpringApplication.run(GestionEnergiaApplication.class, args);
 	}
 }
 
